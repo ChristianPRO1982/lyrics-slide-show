f8dc5074654020d21243c5f446ebe19bce9e2fa8
Christian Martiny <christian.martiny@outlook.com>
2024-10-22 17:18:51 +0200

    .

--- app_main/models.py
+++ app_main/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Song(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255, blank=False, null=False)
-    description = models.TextField(blank=False, null=False)
+    title = models.CharField(max_length=255, unique=True, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
--- app_main/views.py
+++ app_main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import Song
 from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Song
 from .forms import SongForm
 
 
@@ -13,10 +14,29 @@ def homepage(request):
     return render(request, 'app_main/homepage.html')
 
 
+@login_required
 def songs(request):
+    title = ''
+    description = ''
+    
+    if request.method == 'POST':
+        form = SongForm(request.POST)
+        if form.is_valid() and 'new_song' in request.POST:
+            form.save()
+        elif Song.objects.filter(title=request.POST.get('title')).exists():
+            title = request.POST.get('title')
+            description = request.POST.get('description')
+    
     all_songs = Song.objects.all().order_by('title')
-    return render(request, 'app_main/songs.html', {'songs': all_songs})
+    
+    return render(request, 'app_main/songs.html', {
+        'songs': all_songs,
+        'title': title,
+        'description': description,
+        })
 
+
+@login_required
 def modify_song(request, song_id):
     song = get_object_or_404(Song, id=song_id)
     if request.method == 'POST':
@@ -28,6 +48,8 @@ def modify_song(request, song_id):
         form = SongForm(instance=song)
     return render(request, 'app_main/modify_song.html', {'form': form, 'song': song})
 
+
+@login_required
 def delete_song(request, song_id):
     song = get_object_or_404(Song, id=song_id)
     if request.method == 'POST':
