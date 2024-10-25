2b282da7a46a9abd65c0689f51c558df111c2d12
Christian Martiny <christian.martiny@outlook.com>
2024-10-25 14:49:20 +0200

    .

--- app_main/POO.py
+++ app_main/POO.py
@@ -7,8 +7,9 @@ class Verse:
 class Chorus(Verse):
     _chorus_counter = 0
 
-    def __init__(self, followed: bool, text: str):
+    def __init__(self, followed: bool, text: str, start: bool):
         Chorus._chorus_counter += 1
         super().__init__(Chorus._chorus_counter, followed, text)
+        self.start = start
         self.num_chorus = self.num_verse
         del self.num_verse
--- app_main/utils.py
+++ app_main/utils.py
@@ -9,29 +9,48 @@ def get_song_lyrics(song_id: int)->str:
     cursor = conn.cursor()
 
     query = f"""
-SELECT 'chorus' type, num_verse, followed, text
+SELECT chorus, num_verse, followed, text
   FROM app_main_verse
  WHERE song_id = {song_id}
-   AND chorus = True
-
- UNION ALL
-
-SELECT 'verse' type, num_verse, followed, text
-  FROM app_main_verse
- WHERE song_id = {song_id}
-   AND chorus = False
     """
     
     cursor.execute(query)
-    verses = cursor.fetchall()
+    choruses_verses = cursor.fetchall()
 
-    chorus = []
+    choruses = []
     verses = []
-    for verse in verses:
-        if verse[0] == 'chorus':
-            chorus.append(Chorus(verse[2], verse[3]))
+    start = True
+    for chorus_verse in choruses_verses:
+        if chorus_verse[0] == True:
+            print('chorus')
+            choruses.append(Chorus(chorus_verse[2], chorus_verse[3], start))
         else:
-            verses.append(Verse(verse[1], verse[2], verse[3]))
+            print('verse')
+            verses.append(Verse(chorus_verse[1], chorus_verse[2], chorus_verse[3]))
+        start = False
 
     conn.close()
-    return "coucou"
+
+    return song_lyrics(choruses, verses)
+
+
+def song_lyrics(choruses, verses)->str:
+    chorus_str = ''
+    start = -1
+
+    for chorus in choruses:
+        if start == -1:
+            start = chorus.start
+        chorus_str += f"<b>{chorus.text}</b>\n\n"
+    
+    if start:
+        lyrics = chorus_str
+    else:
+        lyrics = ''
+    
+    for verse in verses:
+        lyrics += f"{verse.text}\n\n"
+        if not verse.followed:
+            lyrics += chorus_str
+
+    return lyrics.replace("\n", "<br>")
