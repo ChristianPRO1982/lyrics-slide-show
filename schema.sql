a064cff0e63e21f6afe6884b4d81f3c627c7aae1
Christian Martiny <christian.martiny@outlook.com>
2025-04-09 09:14:45 +0200

    DB

--- schema.sql
+++ schema.sql
@@ -203,7 +203,7 @@ CREATE TABLE `c_groups` (
   PRIMARY KEY (`group_id`),
   UNIQUE KEY `c_groups_unique` (`name`),
   KEY `c_groups_private_key_IDX` (`token`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -656,4 +656,4 @@ CREATE TABLE `socialaccount_socialtoken` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-07 15:18:51
+-- Dump completed on 2025-04-09  9:14:35
