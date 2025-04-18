957069f7a8648f7d81458cc62c32e35f286be978
Christian Martiny <christian.martiny@outlook.com>
2025-04-18 16:40:28 +0200

    fix: docker-compose

--- lyrics_slide_show/settings/prod.py
+++ lyrics_slide_show/settings/prod.py
@@ -4,10 +4,10 @@ print('§§§§§§§§§§§§§§§§§')
 print('§§§ MODE PROD §§§')
 print('§§§§§§§§§§§§§§§§§')
 
-os.environ["DB_FUNCTIONAL_HOST"] = os.getenv('CARTHOGRAPHIE_FUNCTIONAL_HOST')
-os.environ["DB_FUNCTIONAL_USER"] = os.getenv('CARTHOGRAPHIE_FUNCTIONAL_USER')
-os.environ["DB_FUNCTIONAL_PASSWORD"] = os.getenv('CARTHOGRAPHIE_FUNCTIONAL_PASSWORD')
-os.environ["DB_FUNCTIONAL_DATABASE"] = os.getenv('CARTHOGRAPHIE_FUNCTIONAL_DATABASE')
-os.environ["DB_FUNCTIONAL_SSL"] = os.getenv('CARTHOGRAPHIE_FUNCTIONAL_SSL')
+os.environ["DB_FUNCTIONAL_HOST"] = os.getenv('DOCKER_MYSQL_HOST')
+os.environ["DB_FUNCTIONAL_USER"] = os.getenv('DOCKER_MYSQL_USER')
+os.environ["DB_FUNCTIONAL_PASSWORD"] = os.getenv('DOCKER_MYSQL_PASSWORD')
+os.environ["DB_FUNCTIONAL_DATABASE"] = os.getenv('DOCKER_MYSQL_DATABASE')
+os.environ["DB_FUNCTIONAL_SSL"] = os.getenv('DOCKER_MYSQL_SSL')
 
 from .base import *
