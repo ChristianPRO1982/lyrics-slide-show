60dcc3f89bc533c17e4938d6ae16bd60590319ce
Christian Martiny <christian.martiny@outlook.com>
2025-08-21 15:25:42 +0200

    fix: company_name_small in global_variables

--- lyrics_slide_show/context_processors.py
+++ lyrics_slide_show/context_processors.py
@@ -1,7 +1,7 @@
 def global_variables(request):
     return {
         'company_name': 'cARThographie',
-        'company_name_small': '✞🎙🎨',
+        'company_name_small': '🏕️✞🎙🎨🪢',
         'author': 'Chris',
         'description': "cARThographie - des outils gratuits pour vos projets",
         'title_before': '',
